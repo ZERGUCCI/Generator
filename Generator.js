96235e083768bac77ece7a4861dd2b98053d91d5
Lucas <lucas.negritto@gmail.com>
2020-05-16 04:42:13 -0700

    flow ready

--- Generator.js
+++ Generator.js
@@ -1,14 +1,56 @@
 var _ = require('lodash');
+var casual = require('casual');
 'use strict';
 
 const fs = require('fs');
 
-let rawdata = fs.readFileSync('test.json');
-let preprofiles = JSON.parse(rawdata);
+let rawdata = fs.readFileSync('schema.json');
+let profile = JSON.parse(rawdata);
 
-var tasknumber = 50;
+userInput = {
+    userAddress: '2341 glen way',
+    catchAll: '@coolshit.com'
 
-console.log(tasknumber);
+}
+//START GENERATION
+
+// Jig address
+var address = jigAddress(profile, userInput)
+profile.delivery.address1 = address 
+
+// fake person
+var person = fakePerson(profile, userInput)
+
+profile.email = person.fakeEmail
+profile.delivery.firstName = person.fakeFirst
+profile.delivery.lastName = person.fakeLast
+profile.phone = person.fakePhone
+
+console.log(profile)
+
+
+
+
+function jigAddress(profile, userInput) {
+    var unjigged = userInput.userAddress + '1234'
+    return unjigged
+    
+}
+
+function fakePerson(profile, userInput) {
+    var fakeLast = casual.last_name
+    var fakeFirst = casual.first_name
+
+    person = {
+        fakeFirst : fakeFirst,
+        fakeLast : fakeLast,
+        fakeEmail : fakeLast + fakeFirst + userInput.catchAll,
+        fakePhone : casual.phone
+    }
+    return person
+
+}
+
 
 
 
